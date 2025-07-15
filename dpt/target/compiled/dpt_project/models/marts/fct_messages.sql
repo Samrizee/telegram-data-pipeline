2972420f9210c94949f4de83e76318e19165963a
samrizee <samrawitzerfu@gmail.com>
2025-07-15 15:55:56 +0300

    Add core SQL models for data marts and staging

---
+++ dpt/target/compiled/dpt_project/models/marts/fct_messages.sql
@@ -0,0 +1,14 @@
+-- models/marts/fct_messages.sql
+
+with messages as (
+    select * from "telegram_db"."telegram_db"."stg_telegram_messages"
+)
+
+select
+    message_id,
+    channel_username as channel_id,
+    date::date as date_day,
+    text,
+    message_length,
+    has_image
+from messages
---
+++ dpt/target/compiled/dpt_project/models/staging/stg_telegram_messages.sql
@@ -0,0 +1,23 @@
+-- models/staging/stg_telegram_messages.sql
+
+with raw as (
+    select * from "telegram_db"."raw"."telegram_messages"
+)
+
+select
+    id as message_id,
+    channel_username,
+    text,
+    date,
+    media_path,
+    
+    -- Derived fields
+    case 
+        when media_path is not null then true
+        else false
+    end as has_image,
+
+    length(text) as message_length
+
+from raw
+where id is not null
